e1c78909bf70b9977d1662cf10f3fb9a169c0f14
Felipe Fadul <felipe.fadul@hotmail.com>
2025-04-26 17:17:34 -0300

    feat(currency-converter): add logic to convert the currency

--- 01-web-development-fundamentals/007-currency-converter/scripts.js
+++ 01-web-development-fundamentals/007-currency-converter/scripts.js
@@ -1,10 +1,33 @@
+const currencyExchangeRate = {
+  USD: { value: 5.71, symbol: "$" },
+  EUR: { value: 6.46, symbol: "€" },
+  GBP: { value: 7.57, symbol: "£" },
+};
+
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+amount.addEventListener("input", () => {
+  amount.value = removeNonDigitCharacters(amount.value);
+});
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const currencyConverted = convertCurrency(
+    amount.value,
+    currencyExchangeRate[currency.value].value,
+    currencyExchangeRate[currency.value].symbol
+  );
+};
 
 function removeNonDigitCharacters(value) {
   const nonDigitCharacterRegex = /\D+/g;
   return value.replace(nonDigitCharacterRegex, "");
 }
 
-amount.addEventListener("input", () => {
-  amount.value = removeNonDigitCharacters(amount.value);
-});
+function convertCurrency(amount, price, symbol) {
+  const convertedValue = amount * price;
+  return `${symbol} ${convertedValue.toFixed(2)}`;
+}
