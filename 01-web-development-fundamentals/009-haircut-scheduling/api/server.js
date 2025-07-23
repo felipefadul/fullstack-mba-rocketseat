d55dfd0681140f8af104418763adad50fbfed788
Felipe Fadul <felipe.fadul@hotmail.com>
2025-07-23 08:09:25 -0300

    chore(haircut-scheduling): setup the json-server deployment
    - Created server.js to configure and run a JSON server.
    - Added "start" script to package.json for easier server initiation.
    - Downgraded json-server dependency to version 0.17.4 for compatibility.
    - Introduced vercel.json for deployment configuration with Vercel.

---
+++ 01-web-development-fundamentals/009-haircut-scheduling/api/server.js
@@ -0,0 +1,28 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join(__dirname, '../server.json')
+const data = fs.readFileSync(filePath, 'utf-8')
+const db = JSON.parse(data)
+const router = jsonServer.router(db)
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+)
+
+server.use(router)
+
+server.listen(3333, () => {
+  console.log('JSON Server is running')
+})
+
+module.exports = server
