638de0305819344ca3274d8bfdaebd28f04eb0f7
Felipe Fadul <felipe.fadul@hotmail.com>
2025-07-23 08:09:25 -0300

    chore(haircut-scheduling): setup the json-server deployment
    - Created server.js to configure and run a JSON server.
    - Added "start" script to package.json for easier server initiation.
    - Downgraded json-server dependency to version 0.17.4 for compatibility.
    - Introduced vercel.json for deployment configuration with Vercel.

--- 01-web-development-fundamentals/009-haircut-scheduling/src/services/api-config.js
+++ 01-web-development-fundamentals/009-haircut-scheduling/src/services/api-config.js
@@ -1,3 +1,10 @@
+const DEV_URL = 'http://localhost:3333'
+const PROD_URL = 'https://haircut-scheduling.vercel.app/api'
+
+const isLocalEnvironment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1'
+
 export const apiConfig = {
-  baseURL: 'http://localhost:3333',
+  baseURL: isLocalEnvironment ? DEV_URL : PROD_URL,
 }
